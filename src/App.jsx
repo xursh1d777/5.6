61730c96088d403b4311ebb34c613db8fe8cce79
Xurshid <khontorayevvv@gmail.com>
2024-02-20 19:46:32 +0500

    uy ishi

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+
+
+function App() {
+  return (
+    <div>
+      <header className="header">
+        <nav className="header__nav container">
+            <a href="" className="header__logo">PROWEB</a>
+            <ul className="header__list">
+                <li className="header__item">
+                    <a href="" className="header__link">Обо мне</a>
+                </li>
+                <li className="header__item">
+                    <a href="" className="header__link">Мои работы</a>
+                </li>
+                <li className="header__item">
+                    <a href="" className="header__link">Контакты</a>
+                </li>
+            </ul>
+        </nav>
+        <div className="header__content">
+            <h1 className="header__title">Добро пожаловать на мой сайт</h1>
+            <button className="header__button">Позвонить</button>
+        </div>
+    </header>
+    <main>
+        <section className="about">
+            <div className="container">
+                <h2 className="about__title">Обо мне</h2>
+                <div className="about__content">
+                    <div className="about__left">
+                    <img src="./images/user.png" alt="user" />
+                    </div>
+                    <div className="about__right">
+                        <p className="about__name">Зубенко Михаил Петрович</p>
+                        <p className="about__text">Я могу делать семейные портреты в студии, работать на показах мод, снимать моделей для рекламы или фотографировать на свадьбах. Отдельное направление -- съёмки предметов и еды, которые заказывают для рекламы или меню ресторанов. Я могу быть криминальным фотографом мест преступлений, охотником за знаменитостями или снимать диких животных для журналов о природе. Я востребованный специалист в новостных изданиях, работаю в паре с журналистом и могу оказаться где угодно: от гламурной вечеринки до места пожара, наводнения и даже вооруженного конфликта. Моя профессия будет востребована и в будущем, ведь 80% успеха фотографии -- это не качество техники, а профессионализм фотографа. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="work">
+            <div className="container">
+                <h2 className="work__title">Мои работы</h2>
+                <div className="work__content">
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                    <div className="work__box">
+                        <img src="./work.png" alt="work" className="work__image" />
+                        <h3 className="work__name">Чиланзар</h3>
+                        <p className="work__text">Впервые фото было изобретено Жозефом Нисефором Ньепсом. Это произошло в 1822 году, Дагер же опубликовал свою работу только через 17 лет. Жозеф Ньепс смог получить первое негативное изображение в 1835 году </p>
+                        <button className="work__button">Посмотреть</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer className="footer">
+        <div className="container">
+            <p className="footer__title">Связаться со мной</p>
+            <div className="footer__contact">
+                <a href="" className="footer__link">
+                    <img src="./facebook.svg" alt="facebook" />
+                </a>
+                <a href="" className="footer__link">
+                    <img src="./instagram.svg" alt="instagram" />
+                </a>
+                <a href="" className="footer__link">
+                    <img src="./telegram.svg" alt="telegram" />
+                </a>
+                <a href="" className="footer__link">
+                    <img src="./mail.svg" alt="mail" />
+                </a>
+                <a href="" className="footer__link">
+                    <img src="./whatsup.svg" alt="whatsup" />
+                </a>
+            </div>
+        </div>
+    </footer>
+    </div>
+  )
+}
+
+export default App
